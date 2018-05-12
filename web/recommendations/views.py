dca2deb63c21f2330851864ac8170e20a7f72402
Michał Piotr Stankiewicz <stinger.dat@gmail.com>
2018-05-12 11:36:56 +0200

    Ugly version of user

--- web/recommendations/views.py
+++ web/recommendations/views.py
@@ -1,13 +1,37 @@
 from django.views.generic import TemplateView
+import pymongo
 
 
-class GameView(TemplateView):
+class BaseView(TemplateView):
+    mongo_path = 'mongodb://localhost:27017/'
+
+    def get_db(self):
+        return pymongo.MongoClient(self.mongo_path)
+
+
+class GameView(BaseView):
     template_name = 'recommendations/game.html'
 
+    example_data = {
+        'title': 'Jazda',
+        'img': {
+            'href': 'https://demotywatory.pl/uploads/1255436121_by_preceleK_600.jpg',
+            'formatters': {
+                '0': '800',
+                '1': '1600',
+            }
+        }
+    }
 
-class SeriesView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = self.example_data
+        return context
+
+
+class SeriesView(BaseView):
     template_name = 'recommendations/series.html'
 
 
-class UserView(TemplateView):
+class UserView(BaseView):
     template_name = 'recommendations/user.html'
