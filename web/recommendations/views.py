01bf1f94268cc0395eacaf1168fa6339ae94f8dd
Paweł Kamiński <pawel.kaminski@altalog.pl>
2018-05-12 17:12:41 +0200

    feat(web): add simple user rec

--- web/recommendations/views.py
+++ web/recommendations/views.py
@@ -70,23 +70,28 @@ class BaseView(TemplateView):
     def user_info(self, user_id):
         with self.get_client() as client:
             db = client[settings.DB_NAME]
-            users_recommendations_collection = db['userRecommendations']
+            users_collection = db['userRecommendations']
             games_collection = db['product']
-            users_recommendations = list(users_recommendations_collection.find({'userId': user_id}))
-            print(users_recommendations)
-            if users_recommendations:
-                users_recommendations = {
-                    game['itemId']: game['score']
-                    for game in users_recommendations[0]['recommendedItems']
-                }
-                games = self.get_games(games_collection, list(users_recommendations.keys()))
-                games = [
-                    dict(game, score=users_recommendations[game['id']])
-                    for game in games
-                ]
-            else:
-                games = []
 
+            query = {'userId': user_id}
+            recommendations = users_collection.find_one(query)
+
+            if not recommendations:
+                return {}
+
+            users_recommendations = {
+                game['itemId']: game['score']
+                for game in recommendations['recommendedItems']
+            }
+
+            plain_games = self.get_games(games_collection, list(users_recommendations.keys()))
+            games = [
+                dict(game, score=users_recommendations[game['id']])
+                for game in plain_games
+            ]
+
+            games = sorted(games, key=lambda game: game['score'], reverse=True)
+            
         return {'games': games}
 
 
@@ -146,6 +151,5 @@ class SearchView(BaseView):
             request_text = request.GET['game_name']
             items = list(collection.find({'$text': {'$search': f'"{request_text}"'}}))
 
-        print(len(items))
         response.context_data['games'] = items
         return response
