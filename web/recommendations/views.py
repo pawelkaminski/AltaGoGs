7388768a3a5012db6545ce29b7346cdf9dac40dd
Michał Piotr Stankiewicz <stinger.dat@gmail.com>
2018-05-12 10:06:45 +0200

    empty endpoints

--- web/recommendations/views.py
+++ web/recommendations/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class GameDetail(APIView):
+    def get(self, request, game_id, format=None):
+        return Response({'hello': 'world'})
+
+
+class SeriesDetail(APIView):
+    def get(self, request, series_id, fromat=None):
+        return Response({'hello': 'world'})
+
+
+class UserDetail(APIView):
+    def get(self, request, user_id, format=None):
+        return Response({'hello': 'world'})
+
+
